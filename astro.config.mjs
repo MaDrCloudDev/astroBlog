a1e0b1e72549321b8bf9d58e531a79c04782c7fd
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2023-02-19 10:10:44 -0700

    tweaks for lighthouse
    tweaks for lighthouse

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from "astro/config";
+import image from "@astrojs/image";
 import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+	integrations: [
+		tailwind(),
+		image({
+			cacheDir: "./.cache/image",
+		}),
+	],
 	markdown: {
 		remarkPlugins: [rehypeAccessibleEmojis],
 	},
